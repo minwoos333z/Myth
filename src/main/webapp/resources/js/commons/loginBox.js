38913dad9b24b8bcde08dbaf8bed216c8b2e8ead
Minwoo Song <minwoodev@nate.com>
2023-01-02 04:04:04 +0900

    리팩터링 : 로그아웃 수정
    코드 오타로 인한 수정

--- src/main/webapp/resources/js/commons/loginBox.js
+++ src/main/webapp/resources/js/commons/loginBox.js
@@ -49,7 +49,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
         $.ajax({
             type: "post",
-            url: "../user/logoutUserPorcess",
+            url: "../user/userLoginProcess",
             data: {
 
             },
